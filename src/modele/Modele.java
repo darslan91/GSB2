2a3a26448de97b8f54103489b9dff6ba18009a0d
tfoulley <tfoulley>
2018-12-11 08:20:42 +0100

    Revert "11/12/2018"
    This reverts commit c0148b3b992dc6579789a81c0dc45207e6791f2b.

--- src/modele/Modele.java
+++ src/modele/Modele.java
@@ -31,8 +31,8 @@ public class Modele {
 		
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			connexion = DriverManager.getConnection("jdbc:mysql://172.16.203.100/2018foulley", "tfoulley", "123456");
-		//	connexion = DriverManager.getConnection("jdbc:mysql://localhost/gsbv2", "root", "");
+	//		connexion = DriverManager.getConnection("jdbc:mysql://172.16.203.100/2018foulley", "tfoulley", "123456");
+			connexion = DriverManager.getConnection("jdbc:mysql://localhost/gsbv2", "root", "");
 			st = connexion.createStatement();
 		} 
 		catch (ClassNotFoundException erreur) {
