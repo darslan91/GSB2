091d960b67a27444ac6e512068aef5114e382487
Foulley <43532110+Foulley@users.noreply.github.com>
2018-12-17 22:01:16 +0100

    17/12/2018
    changement test liste non incrementé

--- src/panel/consulter/Panel_Fiche_Rembourser.java
+++ src/panel/consulter/Panel_Fiche_Rembourser.java
@@ -127,18 +127,19 @@ public class Panel_Fiche_Rembourser extends JPanel implements FocusListener{
 
 
 	public void focusGained(FocusEvent arg0) {
-		// TODO Auto-genera ted method stub
+		
+			//Valeur de la ligne
 		int val = this.table.getSelectedRow();
 		
 			//récupération des éléments à passer dans le constructeur pour pas regénérer le tableau dans le actionConsulterDétail
 		Object nom = data[val][0];
 		Object prenom = data[val][1];
-		Object mois = data[val][2];
+		Object mois = data[val][2];		
 		
-	//	System.out.println(val);
-		this.revalidate();
-	//	this.btnConsulter.removeActionListener(new ActionConsulterDetailFicheRembourser(this.vue, val));
+		this.remove(this.btnConsulter);
+		this.btnConsulter = new JButton("Consulter");
 		this.btnConsulter.addActionListener(new ActionConsulterDetailFicheRembourser(this.vue, nom, prenom, mois));
+		
 	}
 
 
