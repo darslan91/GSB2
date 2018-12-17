b2e284764d342048935f3051b8fef49ef42f90e2
Foulley <43532110+Foulley@users.noreply.github.com>
2018-12-17 22:02:30 +0100

    17/12/2018
    correctif btn consulter remboursement

--- src/panel/consulter/Panel_Fiche_Rembourser.java
+++ src/panel/consulter/Panel_Fiche_Rembourser.java
@@ -136,8 +136,18 @@ public class Panel_Fiche_Rembourser extends JPanel implements FocusListener{
 		Object prenom = data[val][1];
 		Object mois = data[val][2];		
 		
+		
+		//GridBagLayout
+		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.BOTH;		
+		
+		c.gridx = 0;
+		c.gridy = 2;
+		c.gridwidth = 1;
 		this.remove(this.btnConsulter);
 		this.btnConsulter = new JButton("Consulter");
+		this.add(this.btnConsulter, c);
 		this.btnConsulter.addActionListener(new ActionConsulterDetailFicheRembourser(this.vue, nom, prenom, mois));
 		
 	}
